97c955eaa5a976c370f40126770a1ca526785e56
Pablo Gijón Mora <pablogijonmora@gmail.com>
2023-12-23 19:47:03 +0100

    add mongodb

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
 export const Config = {
-  HTTP_PORT: Number(process.env.PORT) || 8080
+  HTTP_PORT: Number(process.env.PORT) || 8080,
+  MONGODB_URI: process.env.MONGODB_URI!,
+  MONGODB_USERNAME: encodeURIComponent(process.env.MONGODB_USERNAME!),
+  MONGODB_PASSWORD: encodeURIComponent(process.env.MONGODB_PASSWORD!),
 }
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,19 @@ import 'reflect-metadata'
 import { install as installSourceMapSupport } from 'source-map-support'
 import { ExpressApi } from './adapters/primary/rest/express'
 import { Config } from './config'
+import { MongoManager } from './adapters/secondary/mongodb'
 
 async function main() {
   // Source mapping => compiled js
   installSourceMapSupport()
+
+  // Mongo database connection
+  const mongoManager = new MongoManager()
+  // CONNECT DATABASE
+  const mongoClient = await mongoManager.connect()
+
+  if (!mongoClient) throw Error('Error in database connection')
+
 
 
   // HTTP adapter configuration
