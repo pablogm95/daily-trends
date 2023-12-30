ceb1dd24f0dd093acc7e096f262982af8f347875
Pablo Gijón Mora <pablogijonmora@gmail.com>
2023-12-30 22:34:39 +0100

    remove unworking component test

--- __tests__/component/api.test.ts
+++
@@ -1,7 +0,0 @@
-import { request } from './utils'
-
-describe('Rest API', () => {
-  it('should response ping endpoint', async () => {
-    await request.get('/status').expect(200)
-  })
-})
--- __tests__/component/utils.ts
+++
@@ -1,4 +0,0 @@
-import { Config } from '@/config'
-import supertest from 'supertest'
-
-export const request = supertest(`http://localhost:${Config.HTTP_PORT}`)
